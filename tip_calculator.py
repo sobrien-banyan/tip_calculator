4d92fa10a3f6373bbdc1b295410a2e66a77b7fde
sobrien-banyan <sobrien@banyanlabs.io>
2022-10-16 10:52:02 -0700

    updates and comments

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,27 +1,35 @@
-from numpy import True_
+# import custom helper functions
 from tip_calculator_helpers import *
 
-
+# main function
 def tip_calculator():
-    its_go_time = 'Yes'
-    while its_go_time == 'Yes':
+    # 'its_go_time' is the conditon for the while loop. At the end of the tip calculation the user is asked it they want to continue
+    its_go_time = 'yes'
+    
+    # while will contiune to run as long as the user wants
+    while its_go_time.lower() == 'yes':
+        # variables used in the function
         cost = cost_of_food_user_input()
         number_of_people = number_of_people_user_input()
         tip = tip_user_input(cost)
         tax = cost * .1
         total_cost = cost
 
+        # if there is a tip add it and the tax else just add tax
         if tip:
             total_cost = total_cost * (1 + tip/100) + tax
         else:
             total_cost = total_cost + tax
 
+        # print total bill to user
         if cost:
             print(f"Total bill: {'{:.2f}'.format(total_cost)}")
 
+        # if number of people is greater than 1 display cost each person will pay
         if number_of_people > 1:
             print(f"Each person should pay ${'{:.2f}'.format(total_cost / number_of_people)}")
 
+        # asking if user want to use the tip calculator again
         its_go_time = input('Please enter Yes to calculate another tip or enter No to stop ')
 
 tip_calculator()
--- tip_calculator_helpers.py
+++ tip_calculator_helpers.py
@@ -1,9 +1,10 @@
+# function to get user input for cost of food and validate input
 def cost_of_food_user_input():
     cost_of_food = input('Please enter cost of food ')
     try:
-        cost_of_food = int(float(cost_of_food))
+        cost_of_food = float(cost_of_food)
         if cost_of_food <= 0:
-            print('Please enter a number larger than 0')
+            print('Please enter an amount larger than 0')
             return cost_of_food_user_input()
         else:
             return cost_of_food
@@ -11,6 +12,7 @@ def cost_of_food_user_input():
         print("You didn't enter valid cost for the food, please try again")
         return cost_of_food_user_input()
 
+# function to get user input for number of people and validate input
 def number_of_people_user_input():
     number_of_people = input('Please enter number of people splitting the bill ')
     try:
@@ -24,10 +26,11 @@ def number_of_people_user_input():
         print("You didn't enter valid number of people splitting the bill, please try again")
         return number_of_people_user_input()
 
+# function to get user input for tip and validate input
 def tip_user_input(cost_of_food):
-    tip = input(f'What percentage of ${cost_of_food} you like to give in the form of a tip? ')
+    tip = input(f'What percentage of ${"{:.2f}".format(cost_of_food)} you like to give in the form of a tip? ')
     try:
-        tip = int(tip)
+        tip = float(tip)
         if tip < 0:
             print('Tip needs to be zero or higher')
             return tip_user_input()
